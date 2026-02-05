7b657bdb543fac60e477f5cf8a8eba4a103b0f4d
Kshitij Londhe <kshitij.londhe01@gmail.com>
2026-02-05 23:57:07 +0530

    Second commit

--- index.js
+++ index.js
@@ -7,18 +7,33 @@ const myServer = http.createServer((req,res) => {
         return;
     } 
     const log = `${Date.now()}: ${req.url} New Req Recieved\n`
-    const myUrl = url.parse(req.url);
-    fs.appendFile("log.txt",log,(err,data)=>{
-        switch(req.url){
-            case '/':
-                res.end("Home Page");
-                break;
-            case '/about':
-                res.end("Kshitij Londhe");
-                break;
-            default:
-                res.end("404 Page Not Found");
+    const myUrl = url.parse(req.url,true);
+    console.log(myUrl);
+    fs.appendFile("log.txt",log,(err,data)=> {
+        switch(myUrl.pathname){
+        case '/':
+            res.end("Home Page");
+            break;
+        case '/about':
+            const username = myUrl.query.name;
+            res.end(`Hi, ${username}`);
+            break;
+        default:
+            res.end("404 Page Not Found");
         }
     });
 });
+//     fs.appendFile("log.txt",log,(err,data)=>{
+//         switch(myUrl.pathname){
+//             case '/':
+//                 res.end("Home Page");
+//                 break;
+//             case '/about':
+//                 res.end("Kshitij Londhe");
+//                 break;
+//             default:
+//                 res.end("404 Page Not Found");
+//         }
+//     });
+// });
             myServer.listen(8000,() =>console.log("Server Started"));
